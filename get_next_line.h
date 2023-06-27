baed16dd0efe2e090c0f6f88aecef49dcf35c2ff
jqueijo- <joaoluissobral@gmail.com>
2023-06-27 13:57:52 +0100

    les gooo

---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jqueijo- <jqueijo-@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/27 13:55:54 by jqueijo-          #+#    #+#             */
+/*   Updated: 2023/06/27 13:57:23 by jqueijo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h> // Memory allocation
+# include <stdarg.h> // va_lists
+# include <string.h> // String manipulation
+# include <stddef.h> // Types and macros like NULL and size_t
+# include <stdio.h> // Printf
+# include <unistd.h> // System calls, write
+# include <limits.h> // Limits, for testing
+
+
+#endif
