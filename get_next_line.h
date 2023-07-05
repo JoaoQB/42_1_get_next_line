951e2e424d3bb1f2cc00099457719d428e8412f4
jqueijo- <joaoluissobral@gmail.com>
2023-07-05 18:10:11 +0100

    added new, better functions

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,11 @@
 /*   By: jqueijo- <jqueijo-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 13:55:54 by jqueijo-          #+#    #+#             */
-/*   Updated: 2023/06/30 15:50:45 by jqueijo-         ###   ########.fr       */
+/*   Updated: 2023/07/05 17:07:44 by jqueijo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-
 # define GET_NEXT_LINE_H
 
 # include <stdlib.h> // Memory allocation
@@ -23,6 +22,15 @@
 # include <limits.h> // Limits, for testing
 # include <fcntl.h> // Open function
 
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 5
+
+# endif
+
 char	*get_next_line(int fd);
+void	*ft_memset(void *s, int c, size_t n);
+void	*ft_calloc(size_t nmemb, size_t size);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
 
 #endif
