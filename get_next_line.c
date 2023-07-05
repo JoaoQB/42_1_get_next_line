a437e2ec8217b54e3eddc7b50a66c250b3c720de
jqueijo- <joaoluissobral@gmail.com>
2023-07-05 18:11:06 +0100

    printing lines until '\n'

--- get_next_line.c
+++ get_next_line.c
@@ -6,9 +6,76 @@
 /*   By: jqueijo- <jqueijo-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 13:55:51 by jqueijo-          #+#    #+#             */
-/*   Updated: 2023/06/30 15:50:33 by jqueijo-         ###   ########.fr       */
+/*   Updated: 2023/07/05 18:07:04 by jqueijo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char	*read_line(int fd, char *buffer)
+{
+	char	*temp;
+	int	rchars;
+
+	temp = ft_calloc((BUFFER_SIZE + 1), 1);
+	rchars = read(fd, temp, BUFFER_SIZE);
+	while (rchars > 0)
+	{
+		if (!buffer)
+			buffer = ft_calloc(BUFFER_SIZE, 1);
+		buffer = ft_strjoin(buffer, temp);
+		if (ft_strchr(buffer, '\n'))
+			break;
+		rchars = read(fd, temp, BUFFER_SIZE);
+	}
+	free (temp);
+	return (buffer);
+}
+
+char	*create_line(char *buffer)
+{
+	char	*line;
+	int		i;
+
+	i = 0;
+	while (*(buffer + i) != '\n')
+		i++;
+	line = ft_calloc(i + 1, 1);
+	i = 0;
+	while (*(buffer + i) && *(buffer + i) != '\n')
+	{
+		*(line + i) = *(buffer + i);
+		i++;
+	}
+	if (*(buffer + i) && *(buffer + i) == '\n')
+		*(line + i) = '\n';
+	return (line);
+
+}
+
 char	*get_next_line(int fd)
-{}
+{
+	static char	*buffer;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buffer = read_line(fd, buffer);
+	line = create_line(buffer);
+	return (line);
+}
+
+int	main(void)
+{
+	int	fd;
+
+	fd = open("file2.txt", O_RDONLY);
+	char	*buffer;
+
+	buffer = get_next_line(fd);
+	printf("2 I have read: \n%s$\n", buffer);
+	buffer = get_next_line(fd);
+	printf("2 I have read: \n%s$\n", buffer);
+	buffer = get_next_line(fd);
+	printf("2 I have read: \n%s$\n", buffer);
+}
