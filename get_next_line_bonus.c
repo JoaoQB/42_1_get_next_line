1c4308812989c1938be9e68cb5504a731cdccfb8
jqueijo- <joaoluissobral@gmail.com>
2023-07-20 12:43:55 +0100

    bonus working

---
+++ get_next_line_bonus.c
@@ -0,0 +1,135 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jqueijo- <jqueijo-@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/11 16:23:26 by jqueijo-          #+#    #+#             */
+/*   Updated: 2023/07/20 12:40:21 by jqueijo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+void	manage_buffer(char *buffer)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	if (ft_strchr(buffer, '\n'))
+	{
+		while (*(buffer + i) != '\n')
+			i++;
+		i++;
+		while (*(buffer + i))
+			*(buffer + j++) = *(buffer + i++);
+		while (*(buffer + j))
+			*(buffer + j++) = '\0';
+	}
+	else
+	{
+		while (i < BUFFER_SIZE)
+		*(buffer + i++) = '\0';
+	}
+}
+
+char	*create_line(char *temp)
+{
+	char	*line;
+	int		i;
+	int		nl;
+
+	i = 0;
+	nl = 1;
+	if (!temp)
+		return (NULL);
+	if (ft_strchr(temp, '\n'))
+		nl = 2;
+	while (*(temp + i) && *(temp + i) != '\n')
+		i++;
+	line = ft_calloc(i + nl, 1);
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (*(temp + i) && *(temp + i) != '\n')
+	{
+		*(line + i) = *(temp + i);
+		i++;
+	}
+	if (nl == 2)
+		*(line + i) = '\n';
+	return (line);
+}
+
+char	*read_line(int fd, char *buffer)
+{
+	char	*temp;
+	int		rchars;
+
+	temp = NULL;
+	if (*buffer)
+	{
+		temp = create_line(buffer);
+		if (ft_strchr(temp, '\n'))
+			return (temp);
+		manage_buffer(buffer);
+	}
+	rchars = read(fd, buffer, BUFFER_SIZE);
+	while (rchars > 0)
+	{
+		if (!temp)
+			temp = ft_calloc(BUFFER_SIZE + 1, 1);
+		temp = ft_strjoin(temp, buffer);
+		if (ft_strchr(temp, '\n'))
+			break ;
+		manage_buffer(buffer);
+		rchars = read(fd, buffer, BUFFER_SIZE);
+	}
+	if (rchars < 0)
+	{
+		free (temp);
+		return (NULL);
+	}
+	return (temp);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	buffer[FOPEN_MAX][BUFFER_SIZE + 1];
+	char		*temp;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || fd > FOPEN_MAX)
+		return (NULL);
+	temp = read_line(fd, buffer[fd]);
+	if (!temp)
+		return (NULL);
+	line = create_line(temp);
+	if (!line)
+		return (NULL);
+	manage_buffer(buffer[fd]);
+	if (temp)
+		free (temp);
+	return (line);
+}
+/*
+int	main(void)
+{
+	int	fd;
+
+	fd = open("file.txt", O_RDONLY);
+	char	*buffer;
+
+	while (1)
+	{
+		buffer = get_next_line(fd);
+		if (!buffer)
+			break ;
+		printf("Read: %s\n", buffer);
+		free(buffer);
+	}
+
+}*/
---
+++ get_next_line_bonus.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line _bonus.h                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jqueijo- <jqueijo-@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/12 14:47:26 by jqueijo-          #+#    #+#             */
+/*   Updated: 2023/07/20 12:39:10 by jqueijo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+# include <stdlib.h>// Memory allocation
+# include <stdarg.h>// va_lists
+# include <string.h>// String manipulation
+# include <stddef.h>// Types and macros like NULL and size_t
+# include <stdio.h>// Printf
+# include <unistd.h>// System calls, write
+# include <limits.h>// Limits, for testing
+# include <fcntl.h>// Open function
+
+void	*ft_memset(void *s, int c, size_t n);
+void	*ft_calloc(size_t nmemb, size_t size);
+size_t	ft_strlen(const char *str);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+void	manage_buffer(char *buffer);
+char	*create_line(char *temp);
+char	*rchars_check(int rchars, char *temp);
+char	*read_line(int fd, char *buffer);
+char	*get_next_line(int fd);
+
+#endif
